116b371aeef86fcd4adebc7ec9182403ea8fab6f
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2023-12-24 13:36:44 +0000

    no-value for the partmeter

--- wikibot.py
+++ wikibot.py
@@ -1,16 +1,15 @@
 from mylib.bot import scrape
 import click
 
-
 @click.command()
 @click.option("--name", prompt="Wikipedia page to scrape", help="Web page to scrape.")
 @click.option(
     "--length", default=5, help="Number of sentences to scrape from Wikipedia."
 )
-def scrapper(name, length):
+def scrapper(name: str, length: int):
     result = scrape(name, sentences=length)
     click.echo(click.style(f"{result}", fg="white"))
 
-
 if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
     scrapper()
